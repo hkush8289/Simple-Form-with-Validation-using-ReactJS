b0954bd4498bf0a3bcdce6ed8f02f4767b241525
kushagra goswami <hkush8289@gmail.com>
2021-09-02 14:49:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from "./components/form"
+import "./App.css";
+const App = () => {
+  return(
+    <div>
+      <Form></Form>
+    </div>
+  )
+}
+export default App;
